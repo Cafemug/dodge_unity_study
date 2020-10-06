78905bf9304f0ecacb7730b21bc51a7f237320d6
cafemug <hyanghope@gmail.com>
2020-10-06 22:28:40 +0900

    make prefab bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbdoy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRigidbdoy = GetCompnent<Rigidbody>();
+        bulletRigidbdoy.velocity = transform.forward * speed;
+
+        Destory(gameObject, 3f);
+    }
+    
+    void Destory(Object obj, float t);
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player"){
+            PlayerController playerController = other.GetCompnent<PlayerController>();
+
+            if(playerController != null){
+                playerController.Die();
+            }
+        }
+    }
+
+  
+}
